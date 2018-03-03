04e629b4a5deb78ac8975ebd9f904a9752accb6b
Mark Harrison <mharrisondesign@mail.com>
2018-03-03 16:30:22 +0000

    Update chat functionality to read and write from txt file

--- run.py
+++ run.py
@@ -3,25 +3,34 @@ from datetime import datetime #python module that will timestamp messages
 from flask import Flask, redirect, render_template, request #redirect to redirect the messages to the list
 
 app = Flask(__name__)
-messages = [] #chat items stored in a list
+#messages = [] #chat items stored in a list. Now removed as we are using a dictionary
+
+def write_to_file(filename, data):
+    """Handle the process of writing data to a text file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
 
 def add_messages(username, message):
-    # Add messages in to the messages list
-    now = datetime.now().strftime("%H:%M:%S") #Timestamp time order
-    messages_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(messages_dict)
-    #messages.append("({}) {}: {}".format(now, username, message)) #brackets act as placeholders for time, user and message
+    # Add messages in to the 'messages' text file 
+    write_to_file("data/messages.txt", "({0}) {1} - {2}\n".format(
+            datetime.now().strftime("%H:%M:%S"),#Timestamp time order 
+            username.title(), 
+            message))
 
 def get_all_messages():
     """Get all of the messages and separate them using a 'br'"""
-    return messages #Now we are using a dictionary we only need them returned.
+    messages = []
+    with open("data/messages.txt", "r") as chat_messages:
+        messages = chat_messages.readlines()
+    return messages #when the serer is stopped, the messages are saved to this txt file so when rebooted the messages are not lost ajd the user will see their message history on si
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
     """Main page with instructions"""
+    
+    #Handle POST request
     if request.method == "POST":
-        with open("data/users.txt", "a") as user_list:
-            user_list.writelines(request.form["username"] + "\n") #opening the users.txt file and writing to it
+        write_to_file("data/users.tst", request.form["username"] + "\n") #opening the users.txt file and writing to it
         return redirect(request.form["username"]) #upon submit, the index page is shown with username submitted
     return render_template("index.html")
     
